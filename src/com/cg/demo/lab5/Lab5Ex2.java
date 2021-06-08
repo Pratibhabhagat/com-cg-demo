751fffaf8de2258d4ee4a53ff6ed489b65669f38
Pratibhabhagat <bhagatpratibha222@gmail.com>
2021-06-08 18:15:16 +0530

    Update Lab5Ex2.java

--- src/com/cg/demo/lab5/Lab5Ex2.java
+++ src/com/cg/demo/lab5/Lab5Ex2.java
@@ -47,7 +47,7 @@ public class Lab5Ex2 {
 		 System.out.println("sdfghnfghbn");
 		 System.out.println("sjkfgh");
 		 System.out.println("sdfghnfghbn");
-		 System.out.println("sjkfgh");
+		 
 		
 }
 	
