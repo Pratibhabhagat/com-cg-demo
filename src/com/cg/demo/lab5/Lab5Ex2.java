f9b5c08a8157f1a59aa734944bd22b7bbbeda94e
Pratibhabhagat <bhagatpratibha222@gmail.com>
2021-06-08 18:06:20 +0530

    comit
    commit

--- src/com/cg/demo/lab5/Lab5Ex2.java
+++ src/com/cg/demo/lab5/Lab5Ex2.java
@@ -39,6 +39,9 @@ public class Lab5Ex2 {
               System.out.println(e2);
          }
 
+
+		 System.out.println("sdfghnfghbn");
+		 System.out.println("sjkfgh");
 		 System.out.println("sdfghnfghbn");
 		 System.out.println("sjkfgh");
 		
